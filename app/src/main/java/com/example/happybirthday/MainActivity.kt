de8f6bf81d29f5a0c05f0047a41b5c7ff82f0558
Aboiubacar Diaby <aboubacardiaby76600@gmail.com>
2025-10-21 15:27:59 -0400

    ini

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Abou",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
