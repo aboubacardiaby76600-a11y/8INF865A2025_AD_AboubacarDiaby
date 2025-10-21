816eebf6cd8c2828993f58eb30cbc43f6424e744
Aboiubacar Diaby <aboubacardiaby76600@gmail.com>
2025-10-21 16:59:33 -0400

    Etape 6

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +32,8 @@ class MainActivity : ComponentActivity() {
 fun GreetingText(message: String, modifier: Modifier = Modifier) {
     Text(
         text = message,
+        fontSize = 65.sp,
+        lineHeight = 116.sp,
         modifier = modifier
     )
 }
