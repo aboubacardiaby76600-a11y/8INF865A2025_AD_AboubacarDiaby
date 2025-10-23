ecc107ef054d2d7887d3a40e7fe355dcea0087ba
Aboiubacar Diaby <aboubacardiaby76600@gmail.com>
2025-10-23 17:46:03 -0400

    Etape 2 = 13

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,7 +48,9 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
     Box(modifier) {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
         GreetingText(
             message = message,
@@ -53,36 +59,3 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
                 .fillMaxSize()
                 .padding(8.dp)
         )
-    }
-}
-
-@Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    androidx.compose.foundation.layout.Column(
-        modifier = modifier
-    ) {
-        Text(
-            text = message,
-            fontSize = 70.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(16.dp)
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun BirthdayCardPreview() {
-    HappyBirthdayTheme {
-        GreetingImage(
-            message = "Happy Birthday Sam!",
-            from = "From Abou"
-        )
-    }
-}
