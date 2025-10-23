3830248e0cc3d5e791ca432c8ff6352f0ab5dfef
Aboiubacar Diaby <aboubacardiaby76600@gmail.com>
2025-10-23 17:56:31 -0400

    Etape 2 = 14

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -59,3 +59,38 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
                 .fillMaxSize()
                 .padding(8.dp)
         )
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingImage(
+            message = "Happy Birthday Sam!",
+            from = "From Abou"
+        )
+    }
+}
