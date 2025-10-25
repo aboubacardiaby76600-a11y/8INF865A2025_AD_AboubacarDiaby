bf76a129d562670e9413287d2733b7358fb75c29
Aboiubacar Diaby <aboubacardiaby76600@gmail.com>
2025-10-24 20:54:05 -0400

    Etape 7

--- Cupcake/basic-android-kotlin-compose-training-cupcake-starter/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ Cupcake/basic-android-kotlin-compose-training-cupcake-starter/app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -2,33 +2,56 @@ package com.example.cupcake.ui
 
 import android.content.Context
 import android.content.Intent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.cupcake.R
 import com.example.cupcake.data.DataSource
 
-enum class CupcakeScreen {
-    Start, Flavor, Pickup, Summary
+enum class CupcakeScreen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Flavor(title = R.string.choose_flavor),
+    Pickup(title = R.string.choose_pickup_date),
+    Summary(title = R.string.order_summary)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CupcakeApp(
     viewModel: OrderViewModel = viewModel()
 ) {
     val navController = rememberNavController()
-    val uiState by viewModel.uiState.collectAsState()
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = CupcakeScreen.valueOf(
+        backStackEntry?.destination?.route ?: CupcakeScreen.Start.name
+    )
 
-    Scaffold { innerPadding ->
+    val uiState by viewModel.uiState.collectAsState()
+    val canNavigateBack = navController.previousBackStackEntry != null
+
+    Scaffold(
+        topBar = {
+            CupcakeAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = canNavigateBack,
+                navigateUp = { navController.navigateUp() }
+            )
+        }
+    ) { innerPadding ->
         NavHost(
             navController = navController,
             startDestination = CupcakeScreen.Start.name,
@@ -102,6 +125,30 @@ fun CupcakeApp(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CupcakeAppBar(
+    currentScreen: CupcakeScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
 }
 
 private fun cancelOrderAndNavigateToStart(
